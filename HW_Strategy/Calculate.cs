f7bf7918853733ecc13e9f17c1e9495af353e0d6
SummerJiang <peishan1216@gmail.com>
2021-05-04 17:47:36 +0800

    計算面積

---
+++ HW_Strategy/Calculate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Strategy
+{
+    class Calculate
+    {
+        public double GetTotalArea(params IShape[] shapes)
+        {
+            if (shapes == null)
+                throw new ArgumentException("Shape not exist");
+            else
+                return Math.Round(shapes.Sum(s => s.GetTotalArea()), 2);
+        }
+
+    }
+    public interface IShape
+    {
+        double GetTotalArea();
+    }
+    public class Square : IShape
+    {
+        public double Side { get; set; }
+        public Square(double side)
+        {
+            this.Side = side;
+        }
+        public double GetTotalArea()
+        {
+            if (this.Side < 0)
+                throw new ArgumentException("Side >0");
+            return this.Side * this.Side;
+        }
+    }
+    public class Rectangle : IShape
+    {
+        public double Lenght { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double lenght, double width)
+        {
+            this.Lenght = lenght;
+            this.Width = width;
+        }
+
+        public double GetTotalArea()
+        {
+            if (this.Lenght < 0 || this.Width < 0)
+                throw new ArgumentException("Lenght or Width >0");
+            return this.Lenght * this.Width;
+        }
+    }
+    public class Circle : IShape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double GetTotalArea()
+        {
+            if (this.Radius < 0 )
+                throw new ArgumentException("Radius >0");
+            return 3.14 * this.Radius;
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public double Tbase { get; set; }
+        public double Theight { get; set; }
+        public Triangle(double tbase, double theight)
+        {
+            this.Tbase = tbase;
+            this.Theight = theight;
+        }
+
+        public double GetTotalArea()
+        {
+            if (this.Tbase < 0 || this.Theight < 0)
+                throw new ArgumentException("Tbase or Theight > 0");
+            return (this.Tbase * this.Theight) / 2;
+        }
+    }
+
+}
---
+++ HW_Strategy/CalculateTest.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace HW_Strategy
+{
+    [TestFixture]
+    class CalculateTest
+    {
+        private readonly Calculate calculate = new Calculate();
+
+        private IShape[] Rhombus { get;  set; }
+
+        [Test]
+        public void Square_with_Side()
+        {
+            Square square = new Square(5);
+            double TotalArea = calculate.GetTotalArea(square);
+            TotalAreaShouldBe(25, TotalArea);
+        }
+        [Test]
+        public void Square_with_NegativeSide()
+        {
+            Square square = new Square(-5);
+            Action getTotalArea = () => calculate.GetTotalArea(square);
+            getTotalArea.Should().Throw<ArgumentException>().WithMessage("Side >0");
+        }
+
+        [Test]
+        public void Rectangle_with__lenght_width()
+        {
+            Rectangle rectangle = new Rectangle(10, 5);
+            double TotalArea = calculate.GetTotalArea(rectangle);
+            TotalAreaShouldBe(50, TotalArea);
+        }
+        [Test]
+        public void Rectangle_with_Negativelenght_width()
+        {
+            Rectangle rectangle = new Rectangle(-10, -5);
+            Action getTotalArea = () => calculate.GetTotalArea(rectangle);
+            getTotalArea.Should().Throw<ArgumentException>().WithMessage("Lenght or Width >0");
+        }
+
+
+        [Test]
+        public void Circle_with_radius()
+        {
+            Circle circle = new Circle(5);
+            double TotalArea = calculate.GetTotalArea(circle);
+            TotalAreaShouldBe(15.7, TotalArea);
+        }
+
+        [Test]
+        public void Circle_with_Negativeradius()
+        {
+            Circle circle = new Circle(-5);
+            Action getTotalArea = () => calculate.GetTotalArea(circle);
+            getTotalArea.Should().Throw<ArgumentException>().WithMessage("Radius >0");
+        }
+        [Test]
+        public void Triangl_with_Base_Height()
+        {
+            Triangle triangle = new Triangle(2, 5);
+            double TotalArea = calculate.GetTotalArea(triangle);
+            TotalAreaShouldBe(5, TotalArea);
+        }
+        [Test]
+        public void Triangl_with_Negative_Base_Height()
+        {
+            Triangle triangle = new Triangle(-2, -5);
+            Action getTotalArea = () => calculate.GetTotalArea(triangle);
+            getTotalArea.Should().Throw<ArgumentException>().WithMessage("Tbase or Theight > 0");
+        }
+
+        [Test]
+        public void AllShape_TotalArea()
+        {
+            Square square = new Square(5);
+            Rectangle rectangle = new Rectangle(10, 5);
+            Circle circle = new Circle(5);
+            Triangle triangle = new Triangle(2, 5);
+
+            double TotalArea = calculate.GetTotalArea(square, rectangle, circle, triangle);
+            TotalAreaShouldBe(95.7, TotalArea);
+        }
+        [Test]
+        public void Shape_not_exist()
+        {
+            Action getTotalArea = () => calculate.GetTotalArea(Rhombus);
+            getTotalArea.Should().Throw<ArgumentException>().WithMessage("Shape not exist");
+
+        }
+
+      
+        private void TotalAreaShouldBe(double expected, double totalArea)
+        {
+            Assert.AreEqual(expected, totalArea);
+        }
+    }
+}
